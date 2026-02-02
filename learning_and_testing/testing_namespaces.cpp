256c43b983cb85914fc21667876e13029451493f
damianfrench <damian_french@hotmail.co.uk>
2026-02-02 14:21:45 +0000

    adding some more functions to namespaces

--- learning_and_testing/testing_namespaces.cpp
+++ learning_and_testing/testing_namespaces.cpp
@@ -13,6 +13,17 @@ namespace Group1
         std::cin >> y;
         std::cout <<"Sum of the numbers: "<< x+y <<std::endl;
     }
+    void subtracting_numbers()
+    {
+        int x;
+        int y;
+        std::cout <<"Subtracting Numbers" << std::endl;
+        std::cout <<"Input a number:" << std::endl;
+        std::cin >> x;
+        std::cout <<"Input a second number" << std::endl;
+        std::cin >> y;
+        std::cout <<"Difference in the numbers: " << x-y <<std::endl;
+    }
 }
 
 namespace Group2
@@ -28,10 +39,23 @@ namespace Group2
         std::cin >> y;
         std::cout <<"Product of the numbers: "<< x*y <<std::endl;
     }
+    void dividing_numbers()
+    {
+        int x;
+        int y;
+        std::cout <<"Dividing Numbers" << std::endl;
+        std::cout <<"Input a number:" << std::endl;
+        std::cin >> x;
+        std::cout <<"Input a second number" << std::endl;
+        std::cin >> y;
+        std::cout <<"Divided numbers: "<< x/y <<std::endl;
+    }
 }
 
 int main()
 {
     Group1::adding_numbers();
     Group2::multiplying_numbers();
+    Group1::subtracting_numbers();
+    Group2::dividing_numbers();
 }
