03cb27ea17df763e63c6b8cca112e58419b18bd4
damianfrench <damian_french@hotmail.co.uk>
2026-02-05 10:09:37 +0000

    working script but with no error catching

--- assignment_1/main_script.cpp
+++ assignment_1/main_script.cpp
@@ -1,29 +1,59 @@
 #include <iostream>
+#include <tuple>
+#include <cmath>
 
 namespace Bohr_atom
 {
-    int atomic_num_input()
+    std::tuple<int, double, double, char> taking_calculation_inputs()
     {
         int Z;
-        std::cout << "Enter the atomic number for your desired atom:" << std::endl;
-        std::cin >> Z;
-        return Z;
+        double n_i;
+        double n_j;
+        char J;
+        std::cout << "Enter the atomic number for your desired atom, and the initial energy levels, and J or e for an output in joules of eV, all seperated by a space (Z n_i n_f J):" << std::endl;
+        std::cin >> Z >> n_i >> n_j >> J ;
+        std::tuple<int, double, double, char> inputs;
+        inputs = std::make_tuple(Z,n_i,n_j,J);
+        return inputs;
     }
-    
-    int principle_qn()
+
+    char taking_continue_input()
     {
-        int n;
-        std::cin >> n;
-        return n;
+        char continuing;
+        std::cout << "Do you want to repeat? (y,n)" << std::endl;
+        std::cin >> continuing;
+        return continuing;
     }
+
+    double Bohr_calculation(std::tuple<int, double, double, char> inputs)
+    {
+        double E=13.6*(std::get<0>(inputs)*std::get<0>(inputs))*((1/(std::get<1>(inputs)*std::get<1>(inputs)))+(1/(std::get<2>(inputs)*std::get<2>(inputs))));
+        if (std::get<3>(inputs)=='J')
+        {
+            return E*(1.6*std::pow(10,-19));
+        }
+        return E;
+    }
+
 }
 
 int main()
 {
-    std::cout << "Enter the principle Quantum Number for the initial electron energy state" << std::endl;
-    int n_i = Bohr_atom::principle_qn();
-    std::cout << "Enter the principle Quantum Number for the final electron energy state" << std::endl;
-    int n_j = Bohr_atom::principle_qn();
-    int Z = Bohr_atom::atomic_num_input();
+    char continuing='y';
+    while (continuing=='y')
+    {
+        std::tuple<int, double, double, char> inputs=Bohr_atom::taking_calculation_inputs();
+        double Energy=Bohr_atom::Bohr_calculation(inputs);
+        if (std::get<3>(inputs)=='J')
+        {
+            std::cout << Energy << 'J' << std::endl;
+        }
+        else
+        {
+            std::cout << Energy << "eV" << std::endl;
+        }
+        continuing = Bohr_atom::taking_continue_input(); 
+    }
     return 0;
+    
 }
